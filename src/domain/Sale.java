090fbb793b2e7d34de37d1db7a40ede613b27429
Marco Fiorito <marcofiorito1@hotmail.com>
2019-11-09 11:58:09 -0300

    domain

---
+++ src/domain/Sale.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Agustin Hernandorena
+ */
+public class Sale {
+    private ArrayList<Product> purchasedProducts;
+    private PointOfSale shopPlace;
+    private double totalPrice;
+    private String ticketNumber;
+    private LocalDateTime purchaseDate;
+    private double fullPayment;
+    private double change;
+    private ArrayList<Package> usedPackagingList;
+    
+
+    public Sale() {
+        purchasedProducts = new ArrayList<>();
+        usedPackagingList = new ArrayList<>();
+    }
+
+    Sale(PointOfSale shopPlace, String ticketNumber, int fullPayment,
+            int totalPrice) {
+        setShopPlace(shopPlace);
+        setTicketNumber(ticketNumber);
+        setFullPayment(fullPayment);
+        setTotalPrice(totalPrice);
+        setChange(totalPrice-fullPayment);
+        purchasedProducts = new ArrayList<>();
+        usedPackagingList = new ArrayList<>();
+        setPurchaseDate(LocalDateTime.now());
+        
+    }
+
+    public ArrayList<Product> getPurchasedProducts() {
+        return purchasedProducts;
+    }
+
+    public void addProductToCart(Product product) {
+        purchasedProducts.add(product);
+    }
+    
+    public void removeProductOfCart(Product product){
+        purchasedProducts.remove(product);
+    }
+
+    public PointOfSale getShopPlace() {
+        return shopPlace;
+    }
+
+    public void setShopPlace(PointOfSale shopPlace) {
+        this.shopPlace = shopPlace;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(String ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public double getFullPayment() {
+        return fullPayment;
+    }
+
+    public LocalDateTime getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDateTime purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public void setFullPayment(int fullPayment) {
+        this.fullPayment = fullPayment;
+    }
+
+    public double getChange() {
+        return change;
+    }
+
+    public void setChange(int change) {
+        this.change = change;
+    }
+
+    public ArrayList<Package> getPackagesUsed() {
+        return usedPackagingList;
+    }
+
+    public void addUsedPackage(Package aPackage) {
+        usedPackagingList.add(aPackage);
+    }
+    
+    public void removeUsedPackage(Package aPackage){
+        usedPackagingList.remove(aPackage);
+    }
+    
+}
