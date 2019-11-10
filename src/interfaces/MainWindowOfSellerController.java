d78e43978dce060006bbe2eb0c658e9bfc6c2a5f
agustinh2000 <51172421+agustinh2000@users.noreply.github.com>
2019-11-10 08:55:20 -0300

    advance in add product logic

--- src/interfaces/MainWindowOfSellerController.java
+++ src/interfaces/MainWindowOfSellerController.java
@@ -5,9 +5,13 @@
  */
 package interfaces;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
-import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
 import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextField;
+import domain.Product;
 import static interfaces.Ecoshop.myPrimaryStage;
 import static interfaces.Ecoshop.mySystem;
 import java.io.IOException;
@@ -15,14 +19,20 @@ import javafx.scene.input.MouseEvent;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 
 /**
@@ -33,17 +43,28 @@ import javafx.scene.text.Text;
 public class MainWindowOfSellerController implements Initializable {
 
     @FXML
-    private JFXCheckBox check;
+    private JFXCheckBox organicCheck;
     @FXML
     private Label bestSeller;
     @FXML
-    private JFXListView availablePackages;
+    private JFXListView<domain.Package> availablePackages;
     @FXML
     private Text agregarProducto;
-
-    public void evento(ActionEvent e) {
-        java.lang.System.out.println("hola");
-    }
+    @FXML
+    private JFXTextField nameTextField;
+    @FXML
+    private JFXTextField originCountryTextField;
+    @FXML
+    private JFXTextField priceTextField;
+    @FXML
+    private JFXTextField materialTextField;
+    @FXML
+    private JFXCheckBox formedForRecycledMaterialCheck;
+    @FXML
+    private StackPane myStackPane;
+    @FXML
+    private AnchorPane mainPane;
+    private int count = mySystem.getProducts().size();
 
     @FXML
     public void evento3(MouseEvent e) throws IOException {
@@ -53,19 +74,56 @@ public class MainWindowOfSellerController implements Initializable {
         myPrimaryStage.show();
     }
 
-    public void evento2(ActionEvent e) {
-        if (check.isSelected()) {
-            java.lang.System.out.println("hola");
+    @FXML
+    public void registerEvent(ActionEvent e) {
+        String name = nameTextField.getText();
+        String originCountry = originCountryTextField.getText();
+        int price = (Integer.parseInt(priceTextField.getText()));
+        String material = materialTextField.getText();
+        boolean isOrganic = false;
+        boolean formedFromRecycledMaterial = false;
+        if (organicCheck.isSelected()) {
+            isOrganic = true;
         }
+        if (formedForRecycledMaterialCheck.isSelected()) {
+            formedFromRecycledMaterial = true;
+        }
+        ObservableList<domain.Package> list = availablePackages.getSelectionModel().getSelectedItems();
+        Product p = new Product(originCountry, isOrganic, formedFromRecycledMaterial, price, material, 20, name);
+        for (int i = 0; i < list.size(); i++) {
+            p.addPackage(list.get(i));
+        }
+        mySystem.addProduct(p);
+        JFXDialogLayout content = new JFXDialogLayout();
+        myStackPane.setMargin(mainPane, new Insets(180, 141, 260, 280));
+        content.setHeading(new Text("Alta de producto"));
+        content.setBody(new Text("El producto se ha registrado correctamente en el sistema"));
+        JFXDialog dialog = new JFXDialog(myStackPane, content, JFXDialog.DialogTransition.CENTER);
+        JFXButton okButton = new JFXButton("Aceptar");
+        okButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                dialog.close();
+                nameTextField.setText("");
+                originCountryTextField.setText("");
+                materialTextField.setText("");
+                priceTextField.setText("");
+                organicCheck.setSelected(false);
+                formedForRecycledMaterialCheck.setSelected(false);
+                availablePackages.getSelectionModel().clearSelection();
+             
+            }
+        });
+        content.setActions(okButton);
+        dialog.show();
+        count++;
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         availablePackages.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         for (int i = 0; i < mySystem.getPackagesList().size(); i++) {
-            Label lbl = new Label(mySystem.getPackagesList().get(i).getName());
-            availablePackages.getItems().add(lbl);
+            availablePackages.getItems().add(mySystem.getPackagesList().get(i));
         }
-
     }
 }
