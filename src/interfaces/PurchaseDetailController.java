9fc6f7dc088dc37ca92db66ecd394d0bf77ae412
Marco Fiorito <marcofiorito1@hotmail.com>
2019-11-16 20:06:00 -0300

    basic navigation between s=tabs

--- src/interfaces/PurchaseDetailController.java
+++ src/interfaces/PurchaseDetailController.java
@@ -1,9 +1,11 @@
 package interfaces;
 
+import com.jfoenix.controls.JFXTabPane;
 import static interfaces.Ecoshop.myPrimaryStage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
@@ -16,13 +18,42 @@ import javafx.scene.input.MouseEvent;
  * @author Marco Fiorito
  */
 public class PurchaseDetailController implements Initializable {
-
+    
+    private Integer currentTab;
+    
+    @FXML
+    JFXTabPane tabPane;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        currentTab = 0;
+        initializeTabs();
+    }
     
-    public void goBackEvent(MouseEvent e) throws IOException {
+    @FXML
+    public void initializeTabs(){
+        tabPane.getTabs().get(currentTab).setDisable(false);
+        tabPane.getSelectionModel().select(currentTab);
+    }
+    
+    @FXML
+    public void nextTabEvent(MouseEvent e) throws IOException{
+        
+        tabPane.getTabs().get(currentTab).setDisable(true);
+        currentTab++;
+        tabPane.getTabs().get(currentTab).setDisable(false);
+        tabPane.getSelectionModel().select(currentTab);
+    }
+      
+    @FXML
+    public void previousTabEvent(MouseEvent e) throws IOException{
+        tabPane.getTabs().get(currentTab).setDisable(true);
+        currentTab--;
+        tabPane.getTabs().get(currentTab).setDisable(false);
+        tabPane.getSelectionModel().select(currentTab);
+    }
+    
+    public void goToHome(MouseEvent e) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("MainWindowOfBuyer.fxml"));
         Scene scene = new Scene(root);
         myPrimaryStage.setScene(scene);
